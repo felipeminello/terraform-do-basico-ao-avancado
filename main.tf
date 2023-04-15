d2b9a9bf1559393d382ca042e8d2618523801990
Felipe Minello <felipeminello@gmail.com>
2023-04-15 18:38:32 -0300

    criando vm na aws

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 
   backend "s3" {
     bucket = "felipeminello-remote-state"
-    key    = "aws-vpc/terraform.tfstate"
+    key    = "aws-vm/terraform.tfstate"
     region = "us-east-1"
   }
 }
