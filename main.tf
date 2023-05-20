337394a418e98ea4bc8d8234c222e5f3bb17579d
Felipe Minello <felipeminello@gmail.com>
2023-05-20 13:20:29 -0300

    depends on

--- main.tf
+++ main.tf
@@ -25,5 +25,7 @@ module "network" {
   source  = "Azure/network/azurerm"
   version = "3.5.0"
 
+  depends_on = [azurerm_resource_group.resource_group]
+
   resource_group_name = azurerm_resource_group.resource_group.name
 }
