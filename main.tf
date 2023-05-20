a321606b2424a9420b75cad6cf711e24b4e9936f
Felipe Minello <felipeminello@gmail.com>
2023-05-20 12:49:30 -0300

    modulos remotos

--- main.tf
+++ main.tf
@@ -3,42 +3,27 @@ terraform {
   required_version = ">= 1.0.0"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.73.0"
-    }
-
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "2.94.0"
     }
   }
 
-  # backend "s3" {
-  #   bucket = "felipeminello-remote-state"
-  #   key    = "aws-vpc/terraform.tfstate"
-  #   region = "us-east-1"
-  # }
-
   backend "azurerm" {
     resource_group_name  = "remote-state"
     storage_account_name = "felipeminelloremotestate"
     container_name       = "remote-state"
-    key                  = "azure-vnet/terraform.tfstate"
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-
-  default_tags {
-    tags = {
-      owner      = "felipeminello"
-      managed-by = "terraform"
-    }
+    key                  = "azure-vm-modulo-remoto/terraform.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
+}
+
+module "network" {
+  source  = "Azure/network/azurerm"
+  version = "3.5.0"
+
+  resource_group_name = azurerm_resource_group.resource_group.name
 }
--- network.tf
+++ network.tf
@@ -1,26 +1,3 @@
-resource "azurerm_resource_group" "resource_group" {
-  name     = "vnet"
-  location = var.location
-
-  tags = local.common_tags
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-terraform"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-  address_space       = ["10.0.0.0/16"]
-
-  tags = local.common_tags
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-terraform"
-  resource_group_name  = azurerm_resource_group.resource_group.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.0.0/24"]
-}
-
 resource "azurerm_network_security_group" "nsg" {
   name                = "nsg-terraform"
   location            = var.location
@@ -42,6 +19,6 @@ resource "azurerm_network_security_group" "nsg" {
 }
 
 resource "azurerm_subnet_network_security_group_association" "snsga" {
-  subnet_id                 = azurerm_subnet.subnet.id
+  subnet_id                 = module.network.vnet_subnets[0]
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
