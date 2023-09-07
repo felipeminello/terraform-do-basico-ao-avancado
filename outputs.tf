51cc823b6909aa58f50f88d1be0b49e94c38846f
Felipe Minello <felipeminello@gmail.com>
2023-09-07 17:06:31 -0300

    count

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,9 @@
-# output "storage_account_id" {
-#   value = azurerm_storage_account.first_storage_account.id
-# }
-
-# output "bucket_account_id" {
-#   value = aws_s3_bucket.first_bucket.id
-# }
-
-# output "subnet_id" {
-#   value = aws_subnet.subnet.id
-# }
-
-# output "security_group_id" {
-#   value = aws_security_group.security_group.id
-# }
-
-output "subnet_id" {
-  value = azurerm_subnet.subnet.id
+output "subnet_0_id" {
+  value = aws_subnet.subnet[0].id
 }
-
-output "vnet_id" {
-  value = azurerm_virtual_network.vnet.id
+output "subnet_1_id" {
+  value = aws_subnet.subnet[1].id
 }
-
-output "security_group_id" {
-  value = azurerm_network_security_group.nsg.id
+output "subnet_2_id" {
+  value = aws_subnet.subnet[2].id
 }
