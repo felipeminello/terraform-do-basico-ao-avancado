98cff9a42158e31decffbf6f1cbea96c121c16d4
Felipe Minello <felipeminello@gmail.com>
2023-09-07 17:19:59 -0300

    for each

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,9 @@
-# output "storage_account_id" {
-#   value = azurerm_storage_account.first_storage_account.id
-# }
-
-# output "bucket_account_id" {
-#   value = aws_s3_bucket.first_bucket.id
-# }
-
-# output "subnet_id" {
-#   value = aws_subnet.subnet.id
-# }
-
-# output "security_group_id" {
-#   value = aws_security_group.security_group.id
-# }
-
-output "subnet_id" {
-  value = azurerm_subnet.subnet.id
+output "location-resource-group-eua" {
+  value = azurerm_resource_group.resource_group["EUA"].location
 }
-
-output "vnet_id" {
-  value = azurerm_virtual_network.vnet.id
+output "location-resource-group-europa" {
+  value = azurerm_resource_group.resource_group["Europa"].location
 }
-
-output "security_group_id" {
-  value = azurerm_network_security_group.nsg.id
+output "location-resource-group-asia" {
+  value = azurerm_resource_group.resource_group["Asia"].location
 }
