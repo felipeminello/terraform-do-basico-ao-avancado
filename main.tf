3cc8293fe461bb3fc1c84c6e45ea139ab54b04f7
Felipe Minello <felipeminello@gmail.com>
2023-09-07 18:33:02 -0300

    provider

--- main.tf
+++ main.tf
@@ -7,24 +7,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "3.73.0"
     }
-
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.94.0"
-    }
   }
-
-  # backend "s3" {
-  #   bucket = "felipeminello-remote-state"
-  #   key    = "aws-vpc/terraform.tfstate"
-  #   region = "us-east-1"
-  # }
-
-  backend "azurerm" {
-    resource_group_name  = "remote-state"
-    storage_account_name = "felipeminelloremotestate"
-    container_name       = "remote-state"
-    key                  = "azure-vnet/terraform.tfstate"
+  backend "s3" {
+    bucket = "felipeminello-remote-state"
+    key    = "aws-vpc-provider/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
@@ -35,10 +22,20 @@ provider "aws" {
     tags = {
       owner      = "felipeminello"
       managed-by = "terraform"
+      region     = "us-east-1"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
+provider "aws" {
+  alias  = "europa"
+  region = "eu-central-1"
+
+  default_tags {
+    tags = {
+      owner      = "felipeminello"
+      managed-by = "terraform"
+      region     = "us-east-1"
+    }
+  }
 }
